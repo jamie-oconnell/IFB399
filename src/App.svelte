9dd915b4f2a629f3b0ab3d1189067ad5d8392bc6
Jamie O'Connell <Jamielukeoconnell@gmail.com>
2022-03-29 19:41:52 +1000

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Card from "./Card.svelte";
+  import Camera from "./Camera/Camera.svelte";
+</script>
+
+<main class="p-5">
+  <div class="flex items-center mb-5">
+    <div class="col">
+      <img src="/images/Logo.svg" alt="" />
+    </div>
+    <div>
+      <img src="/images/GovLogo.svg" alt="" />
+    </div>
+  </div>
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  <Camera />
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  .col {
+    flex-basis: 50%;
+    flex: 1;
+  }
+</style>
---
+++ src/Camera/CameraCapture.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let mediaStream;
+  export let previewHeight = "";
+  export let previewWidth = "";
+  function srcObject(node, stream) {
+    node.srcObject = stream;
+    return {
+      update(newStream) {
+        if (node.srcObject != newStream) {
+          node.srcObject = newStream;
+        }
+      },
+    };
+  }
+</script>
+
+<video
+  use:srcObject={mediaStream}
+  width={previewWidth}
+  height={previewHeight}
+  autoplay
+  playsinline
+/>
