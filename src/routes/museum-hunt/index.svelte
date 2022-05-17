f72f3a03e99f46bbf2c14c115e575e129b35bcca
SeanBarber-n9741275 <sean.barber@connect.qut.edu.au>
2022-05-17 15:27:02 +1000

    Rough implementation of Treasure Hunt

---
+++ src/routes/museum-hunt/index.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Heading from '$lib/components/Heading.svelte';
+</script>
+
+<Heading>MUSEUM HUNT</Heading>
+
+<div class="container p-5">
+	<Button type="link" link ="/museum-hunt/question-1">START HUNT</Button>
+</div>
+
+<style>
+	.container{
+		background-image: url('/images/Interior.png');
+		background-size:cover;
+		height: 100%;
+		width: 100%;
+	}
+</style>
+
+
---
+++ src/routes/museum-hunt/question-3.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Heading from '$lib/components/Heading.svelte';
+</script>
+
+<Heading>QUESTION 3</Heading>
+<div class="container p-5">
+    <div class="question">
+        <p class="question-box">How much does the large mass of Gypsum weigh? <br />
+                                <br />
+                                The answer can be found on Level 1.5!</p>
+    </div>
+	<div class="buttons">
+		<Button type="link" link="/museum-hunt/question-3-correct/">A) 160kg</Button>
+        <Button type="link" link="/museum-hunt/question-3-incorrect/">B) 200kg</Button>
+		<Button type="link" link="/museum-hunt/question-3-incorrect/">C) 340kg</Button>
+		<Button type="link" link="/museum-hunt/question-3-incorrect/">D) 100kg</Button>
+	</div>
+</div>
+
+<style>
+	.container {
+		background-image: url('/images/Gypsum.png');
+		min-height: 100%;
+		height: 100vh;
+		background-repeat: no-repeat;
+		background-position: center;
+		display: flex;
+		padding-top: 100px;
+		text-align: center;
+		flex-direction: column;
+	}
+
+	.buttons {
+		margin-top: auto;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100px;
+        font-size: 12px;
+	}
+
+    .question-box{
+       background-color: white;
+	   color: black;
+	   border-radius: 25px;
+	   min-height: fit-content;
+	   font-size: 35px;
+	   box-shadow: 0px 6px 9px rgba(0, 0, 0, 0.43);
+    }
+</style>
