2ccb24e9371488c62fbc3fdd959f9152a708c956
Alex-Mouatt <83258182+Alex-Mouatt@users.noreply.github.com>
2022-06-01 20:50:26 +1000

    Added Gallery Tabs

--- src/routes/art-and-culture/virtual-gallery.svelte
+++ src/routes/art-and-culture/virtual-gallery.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Card from '$lib/components/GalleryCard.svelte';
 	import { onMount } from 'svelte';
+	import { Tabs, TabList, TabPanel, Tab } from 'svelte-tabs';
+	import Button from '$lib/components/Button.svelte';
 
 	let images = [];
 	let compimages = [];
@@ -25,21 +27,51 @@
 	});
 </script>
 
-<div class="gallery-grid">
-	{#each images as image}
-		<Card title={image.title} image={image.path} link="/temp" />
-	{/each}
-</div>
-<hr>
-<div class="gallery-grid">
-	{#each compimages as image}
-		<Card title={image.title} image={image.path} link="/temp" />
-	{/each}
-</div>
+<Tabs>
+	<TabList>
+		<Tab>Local Art</Tab>
+		<Tab>Competition Art</Tab>
+	</TabList>
+	<TabPanel>
+		<div class="gallery-grid">
+			{#each images as image}
+				<Card title={image.title} image={image.path} link="/temp" />
+			{/each}
+		</div>
+		<div class='go-back'>
+			<Button type="link" link="/art-and-culture">GO BACK</Button>
+		</div>
+		
+	</TabPanel>
+
+	<TabPanel>
+		<div class="gallery-grid">
+			{#each compimages as image}
+				<Card title={image.title} image={image.path} link="/temp" />
+			{/each}
+		</div>
+		<Button type="link" link="/art-and-culture">GO BACK</Button>
+	</TabPanel>
+</Tabs>
+	
 
 <style>
 	.gallery-grid {
 		display: grid;
 		grid-template-columns: repeat(4, 1fr);
+	}
+	:global(.svelte-tabs li.svelte-tabs__tab){
+		color:white;
+		font-size:larger;
+	}
+	:global(.svelte-tabs){
+		color:white
+	}
+	:global(.svelte-tabs li.svelte-tabs__selected){
+		color:steelblue
+	}
+	.go-back {
+		position:sticky;
+		bottom:0;
 	}
 </style>
